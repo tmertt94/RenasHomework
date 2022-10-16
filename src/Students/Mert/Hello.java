39c90a44c275ed714f8b78353bf06409195ef405
erkankoyun <77962856+erkankoyun@users.noreply.github.com>
2022-10-16 04:35:55 -0400

    Update Hello.java

--- src/Students/Mert/Hello.java
+++ src/Students/Mert/Hello.java
@@ -1,4 +1,25 @@
 package Students.Mert;
 
 public class Hello {
+  public static void main(String[] args) {
+        // next() == > It will read a word from the console
+        // Java Class
+        // nextLine() ==> It reads the whole line on the console.
+        // Java Class
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter you first name: ");
+        String firstName = scan.next();
+        System.out.println("your firstname is: "+firstName);
+        System.out.println("please enter your age: ");
+        String age = scan.next();
+        System.out.println("your age is: "+age);
+        System.out.println("please enter your zipcode: ");
+        String zipcode = scan.next();
+        String zipcodeAge = age+zipcode;
+        System.out.println("the age + Zipcode: "+zipcodeAge);
+
+
+    }
 }
